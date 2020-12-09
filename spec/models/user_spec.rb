784b706aa40a213a2213cb0d6cd102e52b791c65
Paul Stonebraker <wpstonebraker@gmail.com>
2020-12-09 14:26:11 -0500

    finish user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-Rspec.describe User, type: model do
+RSpec.describe User, type: :model do
     it {should validate_presence_of(:username)}
     it {should validate_presence_of(:password_digest)}
     it {should validate_presence_of(:session_token)}
     it {should validate_length_of(:password).is_at_least(6)}
+    
     describe "uniqueness" do
         before(:each) do
             create(:user)
@@ -13,9 +14,49 @@ Rspec.describe User, type: model do
         it {should validate_uniqueness_of(:session_token)}
     end  
     
-    describe "reset_session_token" do
+    describe "#reset_session_token!" do
         let!(:user) {create(:user)}
-        it
+
+        it "should change a user's sesion token" do
+            token = user.session_token
+            expect(user.reset_session_token!).to_not eq(token)
+        end
     end
+
+    describe "::find_by_credentials" do
+        let!(:user) {create(:user)}
+
+        context "user exists" do
+            it "should find the user by username and password" do
+                expect(User.find_by_credentials(user.username, user.password)).to be(user)
+            end
+        end
+
+        context "could not find user" do
+            it "should find the user by username and password" do
+                expect(User.find_by_credentials("usersdf", "sword235")).to eq(nil)
+            end
+        end
     end
+
+    describe "::generate_session_token" do
+        it "should return a string of length 14" do
+            expect(User.generate_session_token.class).to be(String)
+            expect(User.generate_session_token.length).to eq(14)
+        end
+    end
+
+    describe "#is_password?" do
+        let!(:user) {create(:user)}
+        context "correct password" do
+            it "is the correct password" do
+                expect(user.is_password?(user.password)).to be true
+            end    
+        end
+        context "incorrect password" do
+            it "is the incorrect password" do
+                expect(user.is_password?("sadassword")).to be false
+            end
+        end
+    end    
 end
